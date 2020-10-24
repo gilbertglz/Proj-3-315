b333843001922153c9c34c9e71353243f76fb08c
Shuyu Wang <62523867+shuyuwang99@users.noreply.github.com>
2020-10-24 17:29:38 -0500

    added time zone drop down list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
-import AddressSearchBar from './components/search_bar/AddressSearchBar.jsx';
+import AddressSearchBar from './components/AddressSearchBar/AddressSearchBar.jsx';
+import TimeZoneDropDown from './components/TimeZoneDropDown/TimeZoneDropDown.jsx';
 //import uuidv4 from 'uuid/v4'
 //use uuidv4() to generate random ids.
 
 function App() {
   return (
     <div className="App">
+      <TimeZoneDropDown />
       <AddressSearchBar />
     </div>
   );
