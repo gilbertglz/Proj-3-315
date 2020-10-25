ddaddc15872124d4d4b8d6ffd3b7c4ba08c4acef
rynbhuiya <53977041+rynbhuiya@users.noreply.github.com>
2020-10-25 16:54:01 -0500

    Cleaned up code

--- client/src/components/OpenNotify_API/OpenNotify.jsx
+++ client/src/components/OpenNotify_API/OpenNotify.jsx
@@ -3,6 +3,7 @@ import PassTimes from './PassTimes';
 import withPassTimesLoading from './withPassTimesLoading';
 
 const OpenNotify = () => {
+    // TODO: Take in latitude/longitute input
     
     const PassTimesLoading = withPassTimesLoading(PassTimes);
     const [appState, setAppstate] = useState ({
@@ -31,43 +32,3 @@ const OpenNotify = () => {
 } 
 
 export default OpenNotify;
-
-// fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3&alt=20&n=5')
-//     .then(response => response.json())
-//     .then(data => console.log(data));
-
-
-
-
-// var request = new XMLHttpRequest()
-
-// request.open('GET', 'https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3&alt=20&n=5', true)
-
-// request.onload = function () {
-//     var data = JSON.parse(this.response)
-//     var arr = []
-
-//     if (request.status >= 200 && request.status < 400) {
-//        Object.keys(data).forEach( (k) => {
-//            console.log(data[k])
-//            arr.push(data[k])
-//        });
-//     }
-//     else {
-//         console.log ('error')
-//     }
-
-//     var obj = [] // Contains ISS passover time objects
-//     Object.keys(arr[2]).forEach( (n) => {
-//         console.log(arr[2].[n])
-//         obj.push(arr[2].[n]) 
-//     });
-
-//     // Printing out duration and risetime for each object
-//     obj.forEach( (element) => {
-//         console.log("duration: " + element.duration + " risetime: " + element.risetime)
-//     });
-    
-// }
-
-// request.send ()
--- client/src/components/OpenNotify_API/PassTimes.js
+++ client/src/components/OpenNotify_API/PassTimes.js
@@ -8,22 +8,24 @@ const PassTimes = (props) => {
 
     var arr = [];
     Object.keys(pts).forEach( (k) => {
-        console.log(pts[k])
+        //console.log(pts[k])
         arr.push(pts[k])
     });
 
     var obj = []; // Contains ISS passover time objects
     Object.keys(arr[2]).forEach( (n) => {
-        console.log(arr[2].[n])
-        obj.push(arr[2].[n]) 
+        //console.log(arr[2].[n])
+        obj.push(arr[2][n]) 
     });
 
     return (
     <ul>
-        <h2>Available Pass Times</h2>
-        {obj.map ((elem) => {
+        <h2>Available ISS Pass Times</h2>
+        {obj.map ((elem) => { // Creates list with duration and risetime
             return (
-               <li>Duration: {elem.duration} Risetime: {elem.risetime}</li>
+               <li key={elem.id}>
+                   <span> Duration: {elem.duration} Risetime: {elem.risetime} </span>
+               </li>
             );
         })}  
     </ul>
