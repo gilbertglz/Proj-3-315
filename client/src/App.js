027284fa46348b0040caed415d6fa12df08f311b
Adil Rasiyani <adil.rasiyani@tamu.edu>
2020-10-25 15:08:05 -0500

    Initialize Results page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Results from './components/pages/Results/Results';
+import Explore from './components/pages/Explore/Explore';
+
 import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/results">
+            <Results/>
+          </Route>
+          <Route path="/explore">
+            <Explore/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
