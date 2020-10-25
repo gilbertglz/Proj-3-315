ddc0ff70158bae4970d8133e3cd4550d2dedd106
Adil Rasiyani <adil.rasiyani@tamu.edu>
2020-10-25 15:07:57 -0500

    Initialize Home page.

---
+++ client/src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../../Title/Title';
+
+const Home = () => {
+    return (
+        <div>
+            <Title>Home</Title>
+        </div>
+    );
+}
+
+export default Home;
