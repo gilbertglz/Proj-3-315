8023a00453a41e0197861982a8769665d66750d1
Adil Rasiyani <adil.rasiyani@tamu.edu>
2020-11-08 21:11:45 -0600

    Removed testing code from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home/Home';
@@ -7,17 +6,12 @@ import Results from './components/pages/Results/Results';
 import Explore from './components/pages/Explore/Explore';
 import Navigation  from './components/navigation/Navigation';
 import styles from './App.module.scss';
-=======
+
 import './App.scss';
-//import Title from './components/Title/Title.jsx';
-import OpenNotify from './components/OpenNotify_API/OpenNotify.jsx';
-import Sun from './components/Sun_API/Sun.jsx'
->>>>>>> a439bfe... Added sun api function
 
 const App = () => {
 
   return (
-<<<<<<< HEAD
     <div className={styles.App} id="outer-container">
       <Router>
         <Navigation className={styles.nav} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
@@ -36,14 +30,6 @@ const App = () => {
         </div>
       </Router>
     </div>
-=======
-    <>
-      <div className="App">
-        <OpenNotify/>
-        <Sun/>
-      </div>
-    </>
->>>>>>> a439bfe... Added sun api function
   );
 }
 
